917cd0b2115626daf7ec67afb035c51e25e6d548
JonathanAndrew <jonandrewstevenson@yahoo.com>
2015-12-09 17:44:48 -0800

    final edit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	
+
 
 //options( 1 - ON , 0 - OFF)  
         var auto_slide = 1;  
@@ -12,49 +12,162 @@ $(document).ready(function() {
         in milliseconds ( multiplied with 1000) '*/
 
 
-  $( ".hamburger" ).click(function() {
 
+function hideNav(){
+  if ($('.hamburger-menu').css('display') != 'inline-block') {
+    $('.links').slideUp();
+  }
+}
 
-    $( ".links" ).slideToggle( "slow", function() {
+$( '.hamburger-menu' ).click(function() {
+  event.preventDefault();
+  hideNav();
+    $( '.links' ).css('display' , 'inline-block');
 
-    });
-
-  });
+});
 
 $('.links .about').click(function( event ) {
 	event.preventDefault();
-	$('.aboutMeBox').slideToggle('#container');
-  $('.resumeBox').hide();
-  $('.portfolioBox').hide();
+  $('#aboutMeBox').show();
+  $('#resumeBox').hide();
+  $('#portfolioBox').hide();
+  $('#contactBox').hide();
+  var menu = $('header').height();
+  var href = $(this).attr('href');
+  var anchor = $(href).offset();
+  console.log(menu,anchor);
+  window.scrollTo(anchor.left, anchor.top - menu );
+  window.scrollTo(0, anchor.top - menu);
+  
+
 });
+
 
 $('.links .resume').click(function( event ) {
 	event.preventDefault();
-	$('#container.background.resumeBox').slideToggle();
-	$('.aboutMeBox').hide();
-	$('.portfolioBox').hide();
+	$('#resumeBox.container.background').show();
+	$('#aboutMeBox').hide();
+	$('#portfolioBox').hide();
+  $('#contactBox').hide();
+  var href = $(this).attr('href');
+  var anchor = $(href).offset();
+  var menu = $('.header.menu').height();
+  window.scrollTo(anchor.left, anchor.top - menu - 30 );
+
+
 });
 
 $('.links .portfolio').click(function( event ) {
 	event.preventDefault();
-	$('#container.portfolioBox').slideToggle();
-	$('.resumeBox').hide();
-	$('.aboutMeBox').hide();
+	$('#portfolioBox.container').show();
+  $('#submit-btn').hide();
+	$('#resumeBox').hide();
+	$('#aboutMeBox').hide();
+  $('#contactBox').hide();
+  var href = $(this).attr('href');
+  var anchor = $(href).offset();
+  var menu = $('.header.menu').height();
+  window.scrollTo(anchor.left, anchor.top - menu - 50);
+
 
 });
 
-$('ul#menu li ul.sub-menu li a').click(function(){
-	event.preventDefault();
-	$('.slider').hide();
-	$('#slider img ').show();
+$('#portfolioBox ul .photography').click(function(){
+  event.preventDefault();
+  $('#submit-btn').show();
 })
+
+$('.links .contact').click(function( event ) {
+  event.preventDefault();
+  $('#contactBox.container').show();
+  $('#portfolioBox').hide();
+  $('#resumeBox').hide();
+  $('#aboutMeBox').hide();
+  var href = $(this).attr('href');
+  var anchor = $(href).offset();
+  var menu = $('.header.menu').height();
+  window.scrollTo(anchor.left, anchor.top - menu -40);
+  
+
+});
+
+$('.since-graduating a').click(function(){
+  event.preventDefault();
+  $('.since-graduating a').addClass('hide');
+  $('#myStory').slideDown();
+  $('#close').removeClass('hide');
+});
+
+$('#close').click(function(){
+  event.preventDefault();
+  $('#myStory').slideUp();
+  $('.since-graduating a').removeClass('hide');
+
+});
+
+//listen for an event when user clicks the submit button
+
+  $('form').on('change',submitCategory);
+
+  event.preventDefault();
+//function to change background image
+
+  var category = ['Abstract','Close-up','Asymmetrical','Kids','Other'];
+
+  for (i = 0; i < category.length; i++) { 
+    $('#submit-btn').append('<option value="' + category[i] + '">' + category[i] + '</option>')
+  }
+
+
+  function submitCategory(){
+
+    //prevent the page from reloading
+
+    event.preventDefault();
+
+    //select the input
+
+
+    var currentCategory = $('#submit-btn').val();
+
+
+    if(currentCategory== "Abstract") {
+      $('.slider').hide();
+      $('#carousel-abstract').slideToggle();
+      $('#carousel-close-up').hide();
+      $('#carousel-asymmetrical').hide();
+      
+      }
+
+    else if(currentCategory== "Close-up" ) {
+      $('.slider').hide();
+      $('#carousel-close-up').slideToggle();
+       $('#carousel-abstract').hide();
+      $('#carousel-asymmetrical').hide();
+      }
+
+    else if(currentCategory== "Asymmetrical") {
+      $('.slider').hide();
+      $('#carousel-asymmetrical').slideToggle(); 
+       $('#carousel-close-up').hide();
+      $('#carousel-abstract').hide();
+      }
+
+    else if(currentCategory== "Kids" ) {
+      $('body').css('background-image' , "url(images/austin.jpg)");
+      }
+
+    else if(currentCategory== "Other" ) {
+      $('body').css('background-image' , "url(images/sydney.jpg)");
+      }
+  }
 
 
 // settings
   var $slider = $('.slider'); // class or id of carousel slider
   var $slide = 'li'; // could also use 'img' if you're not using a ul
   var $transition_time = 1000; // 1 second
-  var $time_between_slides = 1000; // 4 seconds
+  var $time_between_slides = 2000; // 4 seconds
 
   function slides(){
     return $slider.find($slide);
@@ -82,13 +195,167 @@ $('ul#menu li ul.sub-menu li a').click(function(){
     , $transition_time +  $time_between_slides 
   );
 
- $('#abstract').click(function( event ) {
-	event.preventDefault();
-	$('#conatiner').hide();
-	
+$(function(){
+      var carousel = $('#carousel-abstract ul');
+      var carouselChild = carousel.find('li');
+      var clickCount = 0;
+      
+      itemWidth = carousel.find('li:first').width()+1; //Including margin
 
-});
+      //Set Carousel width so it won't wrap
+      carousel.width(itemWidth*carouselChild.length);
+
+      //Place the child elements to their original locations.
+      refreshChildPosition();
+      
+      //Set the event handlers for buttons.
+      $('.btnNext').click(function(){
+        clickCount++;
+        
+        //Animate the slider to left as item width 
+        carousel.finish().animate({
+          left : '-='+itemWidth
+        },300, function(){
+          //Find the first item and append it as the last item.
+          lastItem = carousel.find('li:first');
+          lastItem.remove().appendTo(carousel);
+          lastItem.css('left', ((carouselChild.length-1)*(itemWidth))+(clickCount*itemWidth));
+        });
+      });
+      
+      $('.btnPrevious').click(function(){
+        clickCount--;
+        //Find the first item and append it as the last item.
+        lastItem = carousel.find('li:last');
+        lastItem.remove().prependTo(carousel);
+
+        lastItem.css('left', itemWidth*clickCount);       
+        //Animate the slider to right as item width 
+        carousel.finish().animate({
+          left: '+='+itemWidth
+        },300);
+      });
+
+      function refreshChildPosition(){
+        carouselChild.each(function(){
+          $(this).css('left', itemWidth*carouselChild.index($(this)));
+        });
+      }
+      
+      function refreshChildPositionNext(){
+        carouselChild.each(function(){
+          leftVal =  parseInt($(this).css('left'));
+        });
+      }
+    });
 
 
+    $(function(){
+      var carousel = $('#carousel-close-up ul');
+      var carouselChild = carousel.find('li');
+      var clickCount = 0;
+      
+      itemWidth = carousel.find('li:first').width()+1; //Including margin
+
+      //Set Carousel width so it won't wrap
+      carousel.width(itemWidth*carouselChild.length);
+
+      //Place the child elements to their original locations.
+      refreshChildPosition();
+      
+      //Set the event handlers for buttons.
+      $('.btnNext').click(function(){
+        clickCount++;
+        
+        //Animate the slider to left as item width 
+        carousel.finish().animate({
+          left : '-='+itemWidth
+        },300, function(){
+          //Find the first item and append it as the last item.
+          lastItem = carousel.find('li:first');
+          lastItem.remove().appendTo(carousel);
+          lastItem.css('left', ((carouselChild.length-1)*(itemWidth))+(clickCount*itemWidth));
+        });
+      });
+      
+      $('.btnPrevious').click(function(){
+        clickCount--;
+        //Find the first item and append it as the last item.
+        lastItem = carousel.find('li:last');
+        lastItem.remove().prependTo(carousel);
+
+        lastItem.css('left', itemWidth*clickCount);       
+        //Animate the slider to right as item width 
+        carousel.finish().animate({
+          left: '+='+itemWidth
+        },300);
+      });
+
+      function refreshChildPosition(){
+        carouselChild.each(function(){
+          $(this).css('left', itemWidth*carouselChild.index($(this)));
+        });
+      }
+      
+      function refreshChildPositionNext(){
+        carouselChild.each(function(){
+          leftVal =  parseInt($(this).css('left'));
+        });
+      }
+    });
+
+$(function(){
+      var carousel = $('#carousel-asymmetrical ul');
+      var carouselChild = carousel.find('li');
+      var clickCount = 0;
+      
+      itemWidth = carousel.find('li:first').width()+1; //Including margin
+
+      //Set Carousel width so it won't wrap
+      carousel.width(itemWidth*carouselChild.length);
+
+      //Place the child elements to their original locations.
+      refreshChildPosition();
+      
+      //Set the event handlers for buttons.
+      $('.btnNext').click(function(){
+        clickCount++;
+        
+        //Animate the slider to left as item width 
+        carousel.finish().animate({
+          left : '-='+itemWidth
+        },300, function(){
+          //Find the first item and append it as the last item.
+          lastItem = carousel.find('li:first');
+          lastItem.remove().appendTo(carousel);
+          lastItem.css('left', ((carouselChild.length-1)*(itemWidth))+(clickCount*itemWidth));
+        });
+      });
+      
+      $('.btnPrevious').click(function(){
+        clickCount--;
+        //Find the first item and append it as the last item.
+        lastItem = carousel.find('li:last');
+        lastItem.remove().prependTo(carousel);
+
+        lastItem.css('left', itemWidth*clickCount);       
+        //Animate the slider to right as item width 
+        carousel.finish().animate({
+          left: '+='+itemWidth
+        },300);
+      });
+
+      function refreshChildPosition(){
+        carouselChild.each(function(){
+          $(this).css('left', itemWidth*carouselChild.index($(this)));
+        });
+      }
+      
+      function refreshChildPositionNext(){
+        carouselChild.each(function(){
+          leftVal =  parseInt($(this).css('left'));
+        });
+      }
+    });
 
 });
